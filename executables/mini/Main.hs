bb24b81baf300f15b53802b738586ac3d5c441f0
Erik Svedäng <erik.svedang@gmail.com>
2015-05-15 23:24:07 +0200

    moved things around a bit

--- executables/mini/Main.hs
+++ executables/mini/Main.hs
@@ -6,8 +6,7 @@ main :: IO ()
 main = run defaultWindow setup draw
 
 setup :: Window -> IO ()
-setup win = do
-  return ()
+setup win = return ()
 
 draw :: () -> IO()
 draw _ = return ()
--- executables/triangles/Main.hs
+++ executables/triangles/Main.hs
@@ -20,6 +20,7 @@ import Foreign.C.String
 import Foreign.C.Types
 import Moose.Boilerplate (run, WindowSettings)
 import Moose.GlHelp (stride, activateAttribute, activateInstanced)
+import Shaders
 
 main :: IO ()
 main = run ("TRIANGLES", 1900, 1200) setup draw
@@ -112,23 +113,4 @@ v3 = GL.Vertex3 (-0.05) 0.05 0.0
 
 v4 :: Vertex3 GLfloat
 v4 = GL.Vertex3 0.05 (-0.05) 0.0
-
-vert = "#version 330 core \
-\layout (location = 0) in vec3 v_position; \
-\layout (location = 1) in float xoffset;\
-\uniform vec3 u_color; \
-\uniform float phaseT; \
-\out vec3 f_color; \
-\void main(void) { \
-\ f_color = u_color; \
-\ float x = v_position.x + xoffset;\
-\ gl_Position = vec4(x, v_position.y + sin(x * 100 + phaseT) * cos(phaseT), v_position.z, 1.0); \
-\}"
-
-frag = "#version 330 core \
-\out vec4 color; \
-\in vec3 f_color; \
-\void main(void) { \
-\ color = vec4(f_color, 1.0f); \
-\}"
 
