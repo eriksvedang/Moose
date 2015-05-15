1ac4ecae5d0964e5eb3a1eb50fe97f9a4ddfca53
Erik Svedäng <erik.svedang@gmail.com>
2015-05-15 15:19:09 +0200

    10k quads + 10k triangles

--- src/Main.hs
+++ src/Main.hs
@@ -20,13 +20,10 @@ import Foreign
 import Foreign.C.String
 import Foreign.C.Types
 
-import System.Remote.Monitoring
-
 onError e message = putStrLn $ "ERROR!" ++ message
 
 main :: IO ()
 main = do
-  forkServer "localhost" 1234
   success <- GLFW.init
   unless success $ putStrLn "Failed to init GLFW."
   GLFW.setErrorCallback (Just onError)
@@ -34,10 +31,11 @@ main = do
   GLFW.windowHint (GLFW.WindowHint'ContextVersionMinor 3)
   GLFW.windowHint (GLFW.WindowHint'OpenGLProfile GLFW.OpenGLProfile'Core);
   GLFW.windowHint (GLFW.WindowHint'OpenGLForwardCompat True);
-  window <- GLFW.createWindow 300 300 "MOOSE" Nothing Nothing
+  window <- GLFW.createWindow 1900 1200 "MOOSE" Nothing Nothing
   case window of
    (Just w) -> do r <- setup w
-                  gameLoop w r
+                  Just t <- GLFW.getTime
+                  gameLoop w r t 0
    Nothing -> do putStrLn "Failed to create window."
                  
 data RenderPass = RenderPass VAO ShaderProgram (RenderPass -> IO ()) BufferObject
@@ -55,8 +53,8 @@ activateInstanced bufferData attributeLocation = do
   glVertexAttribPointer
     attributeLocation
     1 -- components per vertex
-    gl_FLOAT -- coord type
-    (fromBool False) -- normalize?
+    gl_FLOAT
+    (fromBool False)
     0 -- stride
     nullPtr 
   glVertexAttribDivisor attributeLocation 1
@@ -65,7 +63,7 @@ setup :: Window -> IO [RenderPass]
 setup window = do
   GLFW.makeContextCurrent (Just window)
   GLFW.setKeyCallback window (Just keyCallback)
-  GL.clearColor $= Color4 0.9 0.9 0.3 1.0
+  GL.clearColor $= Color4 0.9 0.95 0.95 1.0
   prog <- simpleShaderProgramBS vert frag
   offsets_vbo <- makeBuffer ArrayBuffer offsets
   vao <- makeVAO $ do
@@ -80,8 +78,9 @@ setup window = do
     GL.currentProgram $= Just (program prog)
     activateAttribute prog "v_position" 3
     activateInstanced offsets_vbo 1
-  return [(RenderPass vao prog rf1 offsets_vbo),
-          (RenderPass vao2 prog rf2 offsets_vbo)]
+  return [(RenderPass vao  prog rf1 offsets_vbo)
+         ,(RenderPass vao2 prog rf2 offsets_vbo)
+         ]
 
 rf1 :: RenderPass -> IO ()
 rf1 (RenderPass _ prog _ offsets_vbo) = do
@@ -91,7 +90,8 @@ rf1 (RenderPass _ prog _ offsets_vbo) = do
       off :: GLfloat
       off = -0.3
   setUniform prog "u_color" col
-  glDrawArraysInstanced gl_TRIANGLE_STRIP 0 4 3
+  setUniform prog "phaseT" ((realToFrac t) :: GLfloat)
+  glDrawArraysInstanced gl_TRIANGLE_STRIP 0 4 quadCount
 
 rf2 :: RenderPass -> IO ()
 rf2 (RenderPass _ prog _ offsets_vbo) = do
@@ -99,21 +99,22 @@ rf2 (RenderPass _ prog _ offsets_vbo) = do
   let col :: Vertex3 GLfloat
       col = GL.Vertex3 1.0 (pulse t 0.5 0.75 5.0) 0.7
   setUniform prog "u_color" col
-  glDrawArraysInstanced gl_TRIANGLE_STRIP 0 4 3
+  setUniform prog "phaseT" ((realToFrac t) :: GLfloat)
+  glDrawArraysInstanced gl_TRIANGLES 0 3 quadCount
 
-fib 0 = 1
-fib 1 = 1
-fib x = fib (x - 2) + fib (x - 1)
+quadCount :: CInt
+quadCount = 10000
 
 keyCallback :: GLFW.KeyCallback
 keyCallback window GLFW.Key'Escape _ GLFW.KeyState'Pressed _ =
   GLFW.setWindowShouldClose window True
-keyCallback window GLFW.Key'Space _ GLFW.KeyState'Pressed _ = do
-  putStrLn $ "fib 40 = " ++ show (fib 40)
 keyCallback window _ _ _ _ = putStrLn "Invalid keyboard input."
 
-gameLoop :: Window -> [RenderPass] -> IO ()
-gameLoop window resources = do
+gameLoop :: Window -> [RenderPass] -> Double -> Integer -> IO ()
+gameLoop window resources t frameCount = do
+  Just newT <- GLFW.getTime
+  let dt = newT - t
+  when (frameCount `mod` 30 == 0) (putStrLn $ "FPS: " ++ show (1.0 / dt))
   close <- GLFW.windowShouldClose window
   if close then do
     GLFW.destroyWindow window
@@ -123,7 +124,7 @@ gameLoop window resources = do
     mapM_ draw resources
     GLFW.swapBuffers window
     GLFW.pollEvents
-    gameLoop window resources
+    gameLoop window resources newT (frameCount + 1)
 
 pulse t low high freq =
   let diff = high - low
@@ -133,41 +134,38 @@ pulse t low high freq =
 draw r@(RenderPass vao prog renderFn offsets_vbo) =
   withVAO vao (renderFn r)
 
-vertices = [v1,v2,v3,
-            v2,v4,v5]
+vertices = [v3,v1,v2,v4]
 
-vertices2 = [v5,v3,v4,
-             v2,v1,v5]
+vertices2 = [v3,v1,v2]
 
 offsets :: [GLfloat]
-offsets = [-0.9, 0.0, 1.0]
+offsets = [-1.0, -0.9998 .. 1.0]
+
+--concat $ [[x,y] | x <- [-1.0, -0.8.. 1.0]
+--                , y <- [-1.0, -0.8.. 1.0]]
 
 v1 :: Vertex3 GLfloat
-v1 = GL.Vertex3 (-0.5) (-0.5) 0.0
+v1 = GL.Vertex3 (-0.05) (-0.05) 0.0
 
 v2 :: Vertex3 GLfloat
-v2 = GL.Vertex3 0.5 0.5 0.0
+v2 = GL.Vertex3 0.05 0.05 0.0
 
 v3 :: Vertex3 GLfloat
-v3 = GL.Vertex3 (-0.5) 0.5 0.0
+v3 = GL.Vertex3 (-0.05) 0.05 0.0
 
 v4 :: Vertex3 GLfloat
-v4 = GL.Vertex3 0.5 (-0.5) 0.0
-
-v5 :: Vertex3 GLfloat
-v5 = GL.Vertex3 0.0 (-0.7) 0.0
-
---float offs[] = float[3](-0.2,0.0,0.3); offs[gl_InstanceID]
---uniform float xoffset;
+v4 = GL.Vertex3 0.05 (-0.05) 0.0
 
 vert = "#version 330 core \
 \layout (location = 0) in vec3 v_position; \
 \layout (location = 1) in float xoffset;\
 \uniform vec3 u_color; \
+\uniform float phaseT; \
 \out vec3 f_color; \
 \void main(void) { \
 \ f_color = u_color; \
-\ gl_Position = vec4(v_position.x + xoffset, v_position.y + xoffset * 0.5, v_position.z, 1.0); \
+\ float x = v_position.x + xoffset;\
+\ gl_Position = vec4(x, v_position.y + sin(x * 100 + phaseT) * cos(phaseT), v_position.z, 1.0); \
 \}"
 
 frag = "#version 330 core \
